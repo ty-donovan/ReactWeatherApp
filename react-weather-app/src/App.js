b97ab0b6470353ebc09f4a3a75b1d0d8830f28da
Tyler <td20147@gmail.com>
2023-04-15 13:51:39 -0400

    initialized project, added basic js files we will need

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        Weather App
+      </header>
+    </div>
+  );
+}
+
+export default App;
